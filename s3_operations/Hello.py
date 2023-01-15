7d3e2c46a9014e21bdb2c882775ac978577e1150
hiteshtiwari55 <hitesh.n.u@gmail.com>
2023-01-15 23:30:48 +0000

    first repo set-up commit

---
+++ s3_operations/Hello.py
@@ -0,0 +1,9 @@
+import pytest
+import pandas as pd
+# sample Test
+def test_order():
+    print("After Conftest")
+    print(pd.__version__)
+    df = pd.DataFrame(range(0, 5))
+    print("Hello!! Git crash course!!")
+    print(df)
